c01c6976d205be276a32f996d2ade90e6ed21373
Saidou Diallo <sinthio2@gmail.com>
2020-10-16 16:11:34 +0000

    apprentissage des hooks avec l'utilisation de useState et useEffect

--- src/NaForm.jsx
+++ src/NaForm.jsx
@@ -32,10 +32,42 @@ export class NaForm extends React.Component{
 export function Omar(){
     let[count,setCount]=useState(0)
     useEffect(()=>{
-        
+
     })
     return<div>
         <p>{count}</p>
         <button onClick={()=>setCount(count+1)}></button>
     </div>
+}
+class FriendStatusWithCounter extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = { count: 0, isOnline: null };
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+}
+
+componentDidMount() {
+    document.title = `You clicked ${this.state.count} times`;
+    ChatAPI.subscribeToFriendStatus(
+    this.props.friend.id,
+    this.handleStatusChange
+    );
+}
+
+componentDidUpdate() {
+    document.title = `You clicked ${this.state.count} times`;
+}
+
+componentWillUnmount() {
+    ChatAPI.unsubscribeFromFriendStatus(
+    this.props.friend.id,
+    this.handleStatusChange
+    );
+}
+
+handleStatusChange(status) {
+    this.setState({
+    isOnline: status.isOnline
+    });
+}
 }
