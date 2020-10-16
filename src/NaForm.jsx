d4ed9987b203ad94cdde0d4eee0490f5663b2756
Saidou Diallo <sinthio2@gmail.com>
2020-10-16 16:29:19 +0000

    apprentissage des hooks avec l'utilisation de useState et useReducer

--- src/NaForm.jsx
+++ src/NaForm.jsx
@@ -71,3 +71,22 @@ handleStatusChange(status) {
     });
 }
 }
+
+function useReducer(reducer,initialState) {
+    const [state, setState] = useState(initialState);
+
+function dispatch(action) {
+    const nextState = reducer(state, action);
+    setState(nextState);
+}
+
+return [state, dispatch];
+}
+function Todos() {
+    const [todos, dispatch] = useReducer(todosReducer, []);
+
+    function handleAddClick(text) {
+    dispatch({ type: 'add', text });
+}
+
+}
